5f8252c74bdb264de3da46f0cf047284eb0d579a
Ted Bogin <tbogin@gmail.com>
2016-06-13 12:45:54 -0700

    Add controllers

---
+++ app/controllers/boards_controller.rb
@@ -0,0 +1,49 @@
+class BoardsController < ApplicationController
+	def index
+		@boards = Board.limit(10)
+	end
+
+	def show
+		@board = Board.find_by(id: params[:id])
+	end
+
+	def new
+		@board = Board.new
+	end
+
+	def edit
+		@board = Board.find_by(id: params[:id])
+
+	end
+
+	def create
+		@board = Board.new(board_params)
+		if @board.save
+			redirect_to @board
+		else
+			render 'new'
+		end
+	end
+
+	def update
+		@board = Board.find_by(id: params[:id])
+    	if @board.update(board_params)
+      		redirect_to @board
+    	else
+      		render 'edit'
+    	end
+	end
+
+	def destroy
+		@board = Board.find_by(id: params[:id])
+		@board.destroy
+		redirect_to boards_path
+	end
+
+private
+
+def board_params
+  params.require(:board).permit(:name)
+end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,19 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
+  resources :users
+
+  resources :boards
+
+  resources :cards, except: [:index]
+
+  resources :comments, except: [:index]
+  
+  get 'labels' => 'labels#new'
+  post 'labels' => 'labels#create'
+  delete 'labels' => 'labels#delete'
+
+
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
