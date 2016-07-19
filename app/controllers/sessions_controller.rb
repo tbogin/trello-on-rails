142c0fe037c674a664bae5f960ca5bb52cd2afb8
Ted Bogin <tbogin@gmail.com>
2016-07-19 15:42:57 -0400

    Add login with homepage redirect

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
     @user = User.find_by(email: params[:session][:email].downcase)
     if @user && @user.authenticate(params[:session][:password])
       session[:id] = @user.id
-      redirect_to @user
+      redirect_to "/"
     else
       flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
